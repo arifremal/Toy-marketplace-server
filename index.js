c441010e461233f454b673771fce66aa8f0051a3
arifremal <arifremal013@gmail.com>
2023-05-19 01:36:49 +0600

    server created

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send('Server Running')
+})
+app.listen(port,()=>{
+    console.log(`server running on post:${port}`);
+})
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.AR_USER}:${process.env.AR_PASS}@aremal.jgqn0oc.mongodb.net/?retryWrites=true&w=majority`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
