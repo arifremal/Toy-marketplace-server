60246a864bb1de59f17838c4f04a05a1f9db7d3a
arifremal <arifremal013@gmail.com>
2023-05-19 02:19:16 +0600

    cors,express,dotenv install done

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.get('/',(req,res)=>{
-    res.send('Server Running')
-})
-app.listen(port,()=>{
-    console.log(`server running on post:${port}`);
-})
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+app.listen(port, () => {
+  console.log(`server running on post:${port}`);
+});
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.AR_USER}:${process.env.AR_PASS}@aremal.jgqn0oc.mongodb.net/?retryWrites=true&w=majority`
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.AR_USER}:${process.env.AR_PASS}@aremal.jgqn0oc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,16 +22,24 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db('toyMart').collection('toys')
+    app.get('/toys',async(req,res)=>{
+        const collect = toyCollection.find()
+        const result = await collect.toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
